d49cd8fe057225a8bfc0336116c8776542d1fafb
iHarryD <anuraagvr555@gmail.com>
2022-02-22 13:47:58 +0530

    added basic stylesheets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+import "./CSS/variables.css";
+import "./CSS/typography.css";
+import "./CSS/utilities.css";
+import "./CSS/generic.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
